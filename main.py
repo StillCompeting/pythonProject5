e0ff50c7d35db10ec0f3ae0e2cd63b562c736f50
paulhopkins <plhcpa@hotmail.com>
2021-12-21 14:11:21 -1000

    2nd Commit

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    foo = 5
+    print(foo)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
